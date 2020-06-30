8af358783c2812a962117305f61bbf6f76845459
Soren Wacker <swacker@ucalgary.ca>
2020-06-30 03:51:40 -0600

    ..

--- lrg_omics/proteomics/tools.py
+++ lrg_omics/proteomics/tools.py
@@ -6,39 +6,35 @@ from .quality_control.maxquant import collect_maxquant_qc_data
 from .pipelines.MQparser import MQparser
 
 def formated_rawtools_data_from(path='/var/www/html/proteomics/files/raw'):
-    try:
-        df = collect_rawtools_qc_data(path).drop_duplicates()
-        df.index = df.iloc[::-1].index
-        df.reset_index(inplace=True)
-        df['RawFilePath'] = df['RawFile'].apply(os.path.dirname)
-        df['RawFile'] = df['RawFile'].apply(os.path.basename)
-        for col in ['Instrument', 
-                    'Ms1Analyzer', 
-                    'Ms2Analyzer', 
-                    'Ms3Analyzer', 
-                    'MedianMassDrift(ppm)',
-                    'IdentificationRate(IDs/Ms2Scan)',
-                    'DigestionEfficiency',
-                    'MissedCleavageRate(/PSM)',
-                    'MedianPeptideScore',
-                    'CutoffDecoyScore(0.05FDR)',
-                    'NumberOfPSMs',
-                    'NumberOfUniquePeptides',
-                    'PsmChargeRatio3to2',
-                    'PsmChargeRatio4to2',
-                    'SearchParameters']:
-            del df[col]
-        df.rename(columns={'index': 'Index'}, inplace=True)
-        df['Day'] = df.DateAcquired.dt.dayofyear.astype(str)
-        df['Week'] = df.DateAcquired.dt.week.astype(str)
-        df['Month'] = df.DateAcquired.dt.month.astype(str)
-        df['Year'] = df.DateAcquired.dt.year.astype(str)
-        df['Month'] = df['Year'] + '-' + df['Month']
-        df['Week'] = df['Year'] + '-' + df['Week']
-        df['Day'] = df['Year'] + '-' + df['Day']
-    except:
-        df = pd.DataFrame()
-        df['RawFile'] = ''
+    df = collect_rawtools_qc_data(path) #.drop_duplicates()
+    df.index = df.iloc[::-1].index
+    df.reset_index(inplace=True)
+    df['RawFilePath'] = df['RawFile'].apply(os.path.dirname)
+    df['RawFile'] = df['RawFile'].apply(os.path.basename)
+    for col in ['Instrument', 
+                'Ms1Analyzer', 
+                'Ms2Analyzer', 
+                'Ms3Analyzer', 
+                'MedianMassDrift(ppm)',
+                'IdentificationRate(IDs/Ms2Scan)',
+                'DigestionEfficiency',
+                'MissedCleavageRate(/PSM)',
+                'MedianPeptideScore',
+                'CutoffDecoyScore(0.05FDR)',
+                'NumberOfPSMs',
+                'NumberOfUniquePeptides',
+                'PsmChargeRatio3to2',
+                'PsmChargeRatio4to2',
+                'SearchParameters']:
+        del df[col]
+    df.rename(columns={'index': 'Index'}, inplace=True)
+    df['Day'] = df.DateAcquired.dt.dayofyear.astype(str)
+    df['Week'] = df.DateAcquired.dt.week.astype(str)
+    df['Month'] = df.DateAcquired.dt.month.astype(str)
+    df['Year'] = df.DateAcquired.dt.year.astype(str)
+    df['Month'] = df['Year'] + '-' + df['Month']
+    df['Week'] = df['Year'] + '-' + df['Week']
+    df['Day'] = df['Year'] + '-' + df['Day']
     return df
     
     
