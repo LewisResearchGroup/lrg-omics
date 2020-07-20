13b29b5b2ac17e4fccad10108e4bd9acea152e7b
Soren Wacker <soerendip@gmail.com>
2020-07-20 15:01:31 -0600

    ..

--- lrg_omics/metabolomics/common.py
+++ lrg_omics/metabolomics/common.py
@@ -2,11 +2,11 @@
 
 import pandas as pd
 
-def read_worklist(fn: String):
+def read_worklist(fn: str):
     worklist = pd.read_csv(fn)
     return worklist
 
-def metadata_from_filename(fn: String):
+def metadata_from_filename(fn: str):
     return pd.DataFrame()
 
 
