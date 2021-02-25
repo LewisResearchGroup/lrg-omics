58e009fdc4c047ef8e0d457fde21e5de3997c407
Soren Wacker <swacker@ucalgary.ca>
2021-02-24 20:56:06 -0700

    Table parser script support .txt

--- scripts/lrg_pd_convert.py
+++ scripts/lrg_pd_convert.py
@@ -10,16 +10,27 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Read ThermoFisher worklist and extract data.')
     parser.add_argument('-f', dest='fn_inp', help='Input ThermoFisher workist file (csv format)')
     parser.add_argument('-o', dest='fn_out', help='Output file name')
+    parser.add_argument('--fo', help='Output format [csv, tsv, xlsx, parquet, hdf]')
     parser.add_argument('--hdf-key', default='Dataset')
+    parser.add_argument('--overwrite', default=False, action='store_true')
     parser.add_argument('--sheet-name')
     parser.add_argument('--sep')
 
     args = parser.parse_args()
 
     fn_inp = P(args.fn_inp)
-    fn_out = P(args.fn_out)
+
+    if args.fn_out is None:
+        if args.fo is None:
+            logging.error('Either -o or --fo has to be provided.')
+            exit()
+        fn_out = fn_inp.with_suffix(f'.{args.fo}')
+    else:
+        fn_out = P(args.fn_out)
 
     assert fn_inp != fn_out
+    
+    assert args.overwrite or not fn_out.is_file, f'{fn_out} exists.'
 
     sep = args.sep
 
