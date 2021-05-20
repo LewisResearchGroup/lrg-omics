18e82e9902510997e4ae66bdeaef49e98e2bd4db
Soren Wacker <soerendip@gmail.com>
2021-05-20 14:47:53 -0600

    MaxquantRunner tests pass

--- scripts/lrg_run_maxquant.py
+++ scripts/lrg_run_maxquant.py
@@ -2,7 +2,7 @@
 
 import argparse
 from os.path import isfile
-from lrg_omics.proteomics.MaxQuantRunner import MaxQuantRunner
+from lrg_omics.proteomics import MaxquantRunner
 
 
 if __name__ == '__main__':
@@ -29,20 +29,29 @@ if __name__ == '__main__':
               '"source .bashrc conda activate omics;...".') )
     parser.add_argument('--maxquantcmd', required=True, 
         help='Command to start MaxQuant e.g. "mono MaxQuantCmd.exe".',)
-    parser.add_argument('--dont-add-raw-name', default=True, action='store_false',
-        help='Do not create sub-directory with name of raw file in output directory.')
+    parser.add_argument('--add-raw-name-to-outdir', default=True, action='store_false',
+        help='Do not add subdirectory raw file name to run directory.')
+    parser.add_argument('--add-uuid-to-rundir', default=True, action='store_false',
+        help='Do not add uuid to run directory.')
+    parser.add_argument('--cleanup', default=False, action='store_true',
+        help='Remove run directory after running MaxQuant')        
     parser.add_argument('--verbose', action='store_true')
     args = parser.parse_args()
 
-    mq = MaxQuantRunner(fasta_file=args.fasta, 
+    mq = MaxquantRunner(fasta_file=args.fasta, 
                         mqpar_file=args.mqpar, 
                         run_dir=args.run_dir, 
                         out_dir=args.out_dir, 
                         sbatch_cmds=args.batch_cmd,
-                        maxquantcmd=args.maxquantcmd)
+                        maxquantcmd=args.maxquantcmd,
+                        verbose=args.verbose,
+                        cleanup=args.cleanup,
+                        add_raw_name_to_outdir=args.add_raw_name_to_outdir,
+                        add_uuid_to_rundir=args.add_uuid_to_rundir
+                        )
 
     for raw_file in args.raw[0]:
         if isfile(raw_file):
-            mq.run(raw_file, cold_run=args.cold_run, rerun=args.rerun, submit=args.submit)
+            mq.run(raw_file, cold_run=args.cold_run, rerun=args.rerun, submit=args.submit, run=True)
         else:
             print('W (file not found):', raw_file)
