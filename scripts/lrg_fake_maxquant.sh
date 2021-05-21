94ba5673803f4e17ecec051a38d52f7849dce3ca
Soren Wacker <soerendip@gmail.com>
2021-05-21 11:45:58 -0600

    tests should run

--- scripts/lrg_fake_maxquant.sh
+++ scripts/lrg_fake_maxquant.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-mkdir combined
-mkdir combined/txt
-touch combined/txt/proteinGroups.txt
-touch combined/txt/peptides.txt
+if [[ $1 = '--version' ]]; then
+    echo "lrg_fake_maxquant.sh v0"
+elif [[ $1 == *.xml ]]; then
+    mkdir combined
+    mkdir combined/txt
+    touch combined/txt/proteinGroups.txt
+    touch combined/txt/peptides.txt
+fi
