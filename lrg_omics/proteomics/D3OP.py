53cef42d7898d7722045cf13d8a0fd6325eec706
Soren Wacker <soerendip@gmail.com>
2021-07-07 18:51:41 -0600

    API changed to upload multiple files in one go

--- lrg_omics/proteomics/D3OP.py
+++ lrg_omics/proteomics/D3OP.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 import requests
 import json
+import logging
 
 class D3OP:
     '''
@@ -11,13 +12,18 @@ class D3OP:
     Example:
     
     d3op = D3OP(host='https://proteomics.resistancedb.org', 
-                uid='your-user-uuid',  # Optional
-                pid='your-pipeline-uuid'  # Optional
+                uid='your-user-uuid',     # Optional, required for upload of RAW files
+                pid='your-pipeline-uuid'  # Optional, required for upload of RAW files
                 )
                 
     d3op.get_projects()
+    
     d3op.get_pipelines(project='lsarp')
+    
     d3op.get_qc_data(project='lsarp', pipeline='staphylococcus-aureus-tmt11', data_range=100)
+    
+    d3op.upload_raw(fns=[list-of-filenames])  # Requires uid and pid
+    
     '''
     
     def __init__(self, 
@@ -40,31 +46,47 @@ class D3OP:
         self.projects = pd.DataFrame(r)
         return self.projects
         
-    def get_pipelines(self, project):
+    def get_pipelines(self, project_slug):
         url = f'{self._host}/api/mq/pipelines'
         headers = {'Content-type': 'application/json'}
-        data = json.dumps( dict(project=project) )
+        data = json.dumps( dict(project=project_slug) )
         r = requests.post(url, data=data, headers=headers).json()
         return pd.DataFrame(r)
 
-    def get_qc_data(self, project, pipeline, columns=None, data_range=None):
+    def get_qc_data(self, project_slug, pipeline_slug, columns=None, data_range=None):
         url = f'{self._host}/api/mq/qc-data'
         headers = {'Content-type': 'application/json'}
         if columns is None: columns = ['Index', 'Date', 'RawFile',  'DateAcquired', 
                                        'Use Downstream','Flagged', 'N_protein_groups']
-        data = json.dumps( dict(project=project, pipeline=pipeline, 
+        data = json.dumps( dict(project=project_slug, pipeline=pipeline_slug, 
                                 columns=columns, data_range=data_range) )
         r = requests.post(url, data=data, headers=headers).json()
         df = pd.DataFrame(r)
         df['DateAcquired'] = pd.to_datetime( df['DateAcquired'] )
         return df
     
-    def submit(self, raw_fn):
-        url = f'{self.host}/api/upload/raw'
-        pipeline = self.pipeline
-        user = self.user
-        with open(raw_fn, 'rb') as file:
-            files = {'orig_file': file}
-            data = {'pipeline': pipeline, 'user': user}
-            if self.verbose: print(data, raw_fn)
-            r = requests.post(url, files=files, data=data)        
+    def upload_raw(self, fns):
+        if isinstance(fns, str): fns = [fns]
+        url = f'{self._host}/api/upload/raw'
+        pipeline = self._pipeline_uuid
+        user = self._user_uuid
+        
+        if (pipeline is None) or (user is None):
+            logging.error('Please, initiate D3PO with user_uuid '\
+                          'and pipeline_uuid to submit RAW files.')
+        for fn in fns:
+            with open(fn, 'rb') as file:
+                files = {'orig_file': file}
+                data = {'pipeline': pipeline, 'user': user}
+                if self._verbose: print(f'Uploading {fn}...', end='')
+                r = requests.post(url, files=files, data=data)
+                status_code = r.status_code
+                if self._verbose:
+                    if status_code == 201: print(' success')
+                    else: print(' failed ([{status_code}])')
+            
+            
+    def download_maxquant_data( project_slug, pipeline_slug, filename):
+        url = f'{self.host}/api/download'
+        
+        
